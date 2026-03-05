0ae70b2e0dc3aa3d5d7349f33a58f197a98529d4
Eric-06-HK <dyp24hk@outlook.com>
2026-03-05 22:30:56 +0800

    change 1

--- Components/HAL/hal_usart.h
+++ Components/HAL/hal_usart.h
@@ -1,22 +1,28 @@
 #include "stm32f10x.h"
 #include "OSAL_Comdef.h"
 #include <stdarg.h>
-#include "stdio.h" 
+#include "stdio.h"
 /***************************************************************************************************
- * ´®¿Úº¯Êý
+ * å‡½æ•°å£°æ˜Ž
  ***************************************************************************************************/
 void _dbg_printf(const char *format,...);
 
-void HalUARTInit ( void );	
+void HalUARTInit ( void );
 
 void HalUASRT1_NVIC_Config(void);
 
 void HalUASRT2_NVIC_Config(void);
 
+void HalUASRT3_NVIC_Config(void);
+
 void HalUSART1_Init(u32 bound);
 
 void HalUSART2_Init(u32 bound);
 
+void HalUSART3_Init(u32 bound);
+
 void HalUSART1_IO_Init(void);
 
 void HalUSART2_IO_Init(void);
+
+void HalUSART3_IO_Init(void);
