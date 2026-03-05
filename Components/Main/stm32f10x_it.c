8b3d18f4eecb06a51c1b6b5d66879bfb52d79dde
Eric-06-HK <dyp24hk@outlook.com>
2026-03-05 22:41:22 +0800

    change 1

--- Components/Main/stm32f10x_it.c
+++ Components/Main/stm32f10x_it.c
@@ -1,11 +1,11 @@
 /**
   ******************************************************************************
-  * @file    Project/STM32F10x_StdPeriph_Template/stm32f10x_it.c 
+  * @file    Project/STM32F10x_StdPeriph_Template/stm32f10x_it.c
   * @author  MCD Application Team
   * @version V3.5.0
   * @date    08-April-2011
   * @brief   Main Interrupt Service Routines.
-  *          This file provides template for all exceptions handler and 
+  *          This file provides template for all exceptions handler and
   *          peripherals interrupt service routine.
   ******************************************************************************
   * @attention
@@ -56,7 +56,7 @@ extern uint8_t Prevent_send_bit;
   */
 void NMI_Handler(void)
 {
-	
+
 }
 
 /**
@@ -69,11 +69,11 @@ void HardFault_Handler(void)
 
 
 
-	/* Go to infinite loop when Hard Fault exception occurs */
-	while (1)
-	{
-		NVIC_SystemReset(); //复位
-	}
+        /* Go to infinite loop when Hard Fault exception occurs */
+        while (1)
+        {
+                NVIC_SystemReset(); //位
+        }
 }
 
 /**
@@ -83,11 +83,11 @@ void HardFault_Handler(void)
   */
 void MemManage_Handler(void)
 {
-	/* o to infinite loop when Memory Manage exception occurs */
-	while (1)
-	{
-//		NVIC_SystemReset(); //复位
-	}
+        /* o to infinite loop when Memory Manage exception occurs */
+        while (1)
+        {
+//              NVIC_SystemReset(); //位
+        }
 }
 
 /**
@@ -99,11 +99,11 @@ void BusFault_Handler(void)
 {
 
 
-	/* Go to infinite loop when Bus Fault exception occurs */
-	while (1)
-	{
-//		NVIC_SystemReset(); //复位
-	}
+        /* Go to infinite loop when Bus Fault exception occurs */
+        while (1)
+        {
+//              NVIC_SystemReset(); //位
+        }
 }
 
 /**
@@ -113,12 +113,12 @@ void BusFault_Handler(void)
   */
 void UsageFault_Handler(void)
 {
-	
-	/* Go to infinite loop when Usage Fault exception occurs */
-	while (1)
-	{
-//		NVIC_SystemReset(); //复位
-	}
+
+        /* Go to infinite loop when Usage Fault exception occurs */
+        while (1)
+        {
+//              NVIC_SystemReset(); //位
+        }
 }
 
 /**
@@ -156,78 +156,93 @@ void PendSV_Handler(void)
 extern void HalDelayTime_Counter(void);
 void SysTick_Handler(void)
 {
-	static int count = 0;
-	local_time32_incr++;//系统时间
-	HalDelayTime_Counter();
-	
-		if(local_time32_incr%3000 == 0)//1s 判断一次
-	{
-			HalAdcGetState();
-	}
-	
-	
-	if(local_time32_incr%100 == 0)//100ms 判断一次
-	{
-		if(sys_data.eth_rst_time != 0) 								//计数10s 恢复出厂模式模块
-		{
+        static int count = 0;
+        local_time32_incr++;//系统时
+        HalDelayTime_Counter();
 
-			if(--sys_data.eth_rst_time == 0)
-			{
-				USART4_printf("复位完毕\r\n");
-				HalEth_Rst_Off();
-			}
-		}
-		else
-		{
-			LED_flicker(count++,10);
-		}
-	}
+                if(local_time32_incr%3000 == 0)//1s
+        {
+                        HalAdcGetState();
+        }
+
+
+        if(local_time32_incr%100 == 0)//100ms
+        {
+                if(sys_data.eth_rst_time != 0)                                                          //10s 指模式模
+                {
+
+                        if(--sys_data.eth_rst_time == 0)
+                        {
+                                USART4_printf("位\r\n");
+                                HalEth_Rst_Off();
+                        }
+                }
+                else
+                {
+                        LED_flicker(count++,10);
+                }
+        }
 }
 
 
 void DMA1_Channel4_IRQHandler(void)
 {
-		if(DMA_GetFlagStatus(DMA1_FLAG_TC4))      
-	{    
-		if(Prevent_send_bit==1)//发送完1包开始计时
-		{
-			Prevent_send_bit=3;//开始计时
-			Wait_time=portGetTickCnt();
-//			USART4_printf("计时 %d\r\n",Wait_time);//回复
-			//开始计时
-		}
-		
-		sys_data.Data_UART_Busy=true;
-		DMA_Cmd(DMA1_Channel4, DISABLE);//数据传输完成，关闭DMA4通道
-		DMA_ClearFlag(DMA1_FLAG_TC4); 	//清除DMA1通道7数据完成标志
-		USART_ClearFlag(USART1,USART_FLAG_TC);
-		USART_DMACmd(USART1, USART_DMAReq_Tx, DISABLE); //关闭USART的DMA发送请求		
-	}		
+                if(DMA_GetFlagStatus(DMA1_FLAG_TC4))
+        {
+                if(Prevent_send_bit==1)//1始
+                {
+                        Prevent_send_bit=3;//始
+                        Wait_time=portGetTickCnt();
+//                      USART4_printf(" %d\r\n",Wait_time);//馗
+                        //始
+                }
+
+                sys_data.Data_UART_Busy=true;
+                DMA_Cmd(DMA1_Channel4, DISABLE);//荽桑乇DMA4通
+                DMA_ClearFlag(DMA1_FLAG_TC4);   //DMA1通7
+                USART_ClearFlag(USART1,USART_FLAG_TC);
+                USART_DMACmd(USART1, USART_DMAReq_Tx, DISABLE); //乇USARTDMA
+        }
 }
 void EXTI15_10_IRQHandler(void)
 {
-	if(EXTI_GetITStatus(HAL_KEY3_EXTI_LIN) != RESET)
-	{
-		if(GPIO_ReadInputDataBit(HAL_KEY3_PORT, HAL_KEY3_PIN) == 0){
-			HalKey3_IT_Disable();//关闭中断线
-			HalTimer4_IT_Enable();//开启定时器1
-		}
-		/* Clear EXTI Line 9 Pending Bit */
-		EXTI_ClearITPendingBit(HAL_KEY3_EXTI_LIN);
-	}
-		if(EXTI_GetITStatus(HAL_KEY2_EXTI_LIN) != RESET)
-	{
-		if(GPIO_ReadInputDataBit(HAL_KEY2_PORT, HAL_KEY2_PIN) == 0){
-//			HalKey3_IT_Disable();//关闭中断线
-//			HalTimer4_IT_Enable();//开启定时器1
-//			USART4_printf("按键按下\r\n");
-		}
-		/* Clear EXTI Line 9 Pending Bit */
-		EXTI_ClearITPendingBit(HAL_KEY2_EXTI_LIN);
-	}
-	
-	
+        if(EXTI_GetITStatus(HAL_KEY3_EXTI_LIN) != RESET)
+        {
+                if(GPIO_ReadInputDataBit(HAL_KEY3_PORT, HAL_KEY3_PIN) == 0){
+                        HalKey3_IT_Disable();//乇卸
+                        HalTimer4_IT_Enable();//时1
+                }
+                /* Clear EXTI Line 9 Pending Bit */
+                EXTI_ClearITPendingBit(HAL_KEY3_EXTI_LIN);
+        }
+                if(EXTI_GetITStatus(HAL_KEY2_EXTI_LIN) != RESET)
+        {
+                if(GPIO_ReadInputDataBit(HAL_KEY2_PORT, HAL_KEY2_PIN) == 0){
+//                      HalKey3_IT_Disable();//乇卸
+//                      HalTimer4_IT_Enable();//时1
+//                      USART4_printf("\r\n");
+                }
+                /* Clear EXTI Line 9 Pending Bit */
+                EXTI_ClearITPendingBit(HAL_KEY2_EXTI_LIN);
+        }
 
+
+
+}
+
+/**
+ * @brief  USART3戏扫枪萁眨
+ */
+#include "shopping_cart.h"
+void USART3_IRQHandler(void)
+{
+        if(USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)
+        {
+                uint8_t data = USART_ReceiveData(USART3);
+                // 荽莞扫枪
+                BarcodeScanner_OnDataReceived(data);
+                USART_ClearITPendingBit(USART3, USART_IT_RXNE);
+        }
 }
 
 
